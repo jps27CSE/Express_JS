9357187a45297e580fcf7405f646d2515449aaca
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-09 13:03:24 +0600

    create multiple route

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.get('/about', (req, res) => {
+    res.send('<h1>i am about page</h1>');
+
+})
+
+app.get('/help',(req, res) => {
+    res.send('<h1>i am help page</h1>');
+})
+
 app.get('/', (req, res) => {
     res.send('<h1>I am Listening</h1>');
 
+})
+
+app.get('*',(req, res) => {
+    res.send('<h1>404 Not Found</h1>')
 })
 
 const PORT = process.env.PORT || 8080
