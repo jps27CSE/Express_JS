6326b4c80713ec0f7dadb23b1e20dd33228b019f
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-09 12:22:38 +0600

    create first route

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('<h1>I am Listening</h1>');
+
+})
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`)
