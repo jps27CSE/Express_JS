31b94d0aca31c2b29dc5b087969bacb29d661793
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-11 00:15:00 +0600

    added prettier config file

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
-const express = require('express');
-const morgan = require('morgan');
-
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 // app.use(morgan('dev'));
 
 function customMiddleware(req, res, next) {
-    if (req.url === '/help') {
-        res.send('<h1>the site is blocked by Administrator</h1>');
-    }
-    next()
+  if (req.url === "/help") {
+    res.send("<h1>the site is blocked by Administrator</h1>");
+  }
+  next();
 }
 
-app.use(customMiddleware)
+app.use(customMiddleware);
 
-app.get('/about', morgan('dev'), (req, res) => {
-    res.send('<h1>i am about page</h1>');
+app.get("/about", morgan("dev"), (req, res) => {
+  res.send("<h1>i am about page</h1>");
+});
 
-})
+app.get("/help", (req, res) => {
+  res.send("<h1>i am help page</h1>");
+});
 
-app.get('/help', (req, res) => {
-    res.send('<h1>i am help page</h1>');
-})
+app.get("/", (req, res) => {
+  res.send("<h1>I am Listening</h1>");
+});
 
-app.get('/', (req, res) => {
-    res.send('<h1>I am Listening</h1>');
+app.get("*", (req, res) => {
+  res.send("<h1>404 Not Found</h1>");
+});
 
-})
-
-app.get('*', (req, res) => {
-    res.send('<h1>404 Not Found</h1>')
-})
-
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on Port ${PORT}`)
-})
+  console.log(`Server is running on Port ${PORT}`);
+});
