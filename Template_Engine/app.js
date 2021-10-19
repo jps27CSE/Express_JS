8b94f32c3c699aa9508974977191e4048cc66bdf
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-19 16:04:31 +0600

    Dynamic Title

--- Template_Engine/app.js
+++ Template_Engine/app.js
@@ -10,11 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/about", (req, res) => {
-  res.render("pages/about");
+  res.render("pages/about", { title: "about" });
 });
 
 app.get("/help", (req, res) => {
-  res.render("pages/help");
+  res.render("pages/help", { title: "help" });
 });
 
 app.get("/", (req, res) => {
