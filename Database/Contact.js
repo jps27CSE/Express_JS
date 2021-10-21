7e9cdc2eb080fbe26766460700cba17abba6c57e
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-21 19:32:34 +0600

    Create Contact Model

--- Database/Contact.js
+++ Database/Contact.js
@@ -1,4 +1,4 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const contactSchema = new Schema({
   name: {
@@ -22,3 +22,7 @@ const contactSchema = new Schema({
     maxlength: 15,
   },
 });
+
+const Contact = model("Contact", contactSchema);
+
+module.exports = Contact;
